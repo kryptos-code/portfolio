79daa830978cadf26349563ba00e3e185e8db4a8
Shoaib Rehman <kryptosshoaib@gmail.com>
2025-05-12 23:18:52 +0530

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,6 @@
+// config.js
+const EMAILJS_PUBLIC_KEY = "Qzu5pM3aES7thWiu-";
+const EMAILJS_SERVICE_ID = "service_3wzd2sr";
+const EMAILJS_TEMPLATE_ID = "template_d6mla1k";
+// reCaptcha Validation
+const RECAPTCHA_SITE_KEY = "6LceQTcrAAAAAKmWHeug-ChKSRN62_HtqI85ZZKt";
